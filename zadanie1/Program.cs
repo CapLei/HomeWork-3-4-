86a98e18a3ea659d500484976e0ddb366dd5d6b0
Vlad Dre <dreier.vlad@mail.ru>
2023-11-29 21:07:53 +0300

    Задание 1 из 3его практического

---
+++ zadanie1/Program.cs
@@ -0,0 +1,47 @@
+﻿
+int [] CreateArray(int min, int max, int size){
+
+    int [] array = new int [size];
+    for(int i = 0; i<size; i++){
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array; 
+}
+
+void ShowArray(int [] array){
+    
+    for(int i = 0; i<array.Length;i++){
+        Console.Write(array[i]+" ");
+    }
+    Console.WriteLine();
+}
+
+int FindDigit(int[] array, int result){
+   result = 0;
+    for(int i = 0; i<array.Length; i++){
+    
+        if(array[i] > 20 && array[i] < 90){
+           
+           result++;
+            
+        }
+    }
+    return result;
+}
+
+
+Console.WriteLine("Enter array size");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter array min");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter array max");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int result=0;
+int [] array = CreateArray(min, max, size);
+ShowArray(array);
+int res = FindDigit(array, result);
+Console.WriteLine(res);
+
+
+
