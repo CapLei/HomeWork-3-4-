4dd6818aa60aadbf1d5634da50de2e02f7f0eeca
Vlad Dre <dreier.vlad@mail.ru>
2023-12-04 18:39:55 +0300

    Задание 4.1

---
+++ ex/Program.cs
@@ -0,0 +1,42 @@
+﻿bool v = true;
+while(v){
+    Console.WriteLine("Enter your number"); 
+    if (Console.ReadLine()==("q")){
+       v = false;
+       break;
+    }else{
+    void Esc2(int number){
+    int num =number;
+    int res = 0 ;
+    while(num>0){
+     res= res + num%10;
+    num/=10;
+    }
+    if(res%2==0){
+    v = false;
+    }
+  }
+     int number = Convert.ToInt32(Console.ReadLine()); 
+  Esc2(number);
+ }
+    
+
+    
+
+    
+}
+
+
+
+/* 
+void Esc2(int number){
+    int num =number;
+    int res = 0 ;
+    while(num>0){
+        res= res + num%10;
+        num/=10;
+    }
+    if(res%2==0){
+        v = false;
+    }
+} */
