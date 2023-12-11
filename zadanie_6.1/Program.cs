2e076937044df0f9681c331f78b4b51757337a88
Vlad Dre <dreier.vlad@mail.ru>
2023-12-11 18:55:33 +0300

    задание 6.1

---
+++ zadanie_6.1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+string CreateStringArray(char[,] array){
+    string res ="";
+    for(int i = 0; i<array.GetLength(0); i++){
+        for(int j = 0; j<array.GetLength(1); j++){
+            res += array[i,j];
+        }
+    }
+    return res;
+}
+
+
+
+char[,] array = new char[,] { {'a','b'} , {'c','d'} };
+string result = CreateStringArray(array);
+Console.WriteLine(result);
